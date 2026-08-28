861d53c743ec321682082d2de9d044249ddada48
Cptzeph23 <asekazephaniah@gmail.com>
2026-08-28 09:19:55 +0300

    urls

--- SMS/SMS/urls.py
+++ SMS/SMS/urls.py
@@ -1,8 +1,13 @@
-
+"""Absolute path: SMS/SMS/urls.py"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('smsApp.urls', namespace='dashboard'))
+    path("admin/", admin.site.urls),
+    path("", include("smsApp.urls")),
+    # Spec §25: versioned API + OpenAPI/Swagger-compatible documentation.
+    path("api/v1/", include("smsApp.api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="api_docs"),
 ]
